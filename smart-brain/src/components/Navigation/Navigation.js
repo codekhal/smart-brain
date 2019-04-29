431a632eefae27f7f30e6f113e915450b9d90fad
khushal <vyas.khushal1999@gmail.com>
2019-04-29 12:01:47 +0530

    added signin functionality

--- smart-brain/src/components/Navigation/Navigation.js
+++ smart-brain/src/components/Navigation/Navigation.js
@@ -4,14 +4,14 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
       if(isSignedIn) {
           return(
         <nav style = {{display: 'flex', justifyContent: 'flex-end'}}>
-            <p onclick = {() => onRouteChange('signout')} className = 'f3 link dim pa3 ba2 br-pill pointer shadow-5'> Sign Out </p>
+            <p onClick = {() => onRouteChange('signout')} className = 'f3 link dim pa3 ba2 br-pill pointer shadow-5'> Sign Out </p>
         </nav>
           );
       } else {
         return(
             <nav style = {{display: 'flex', justifyContent: 'flex-end'}}>
-                <p onclick = {() => onRouteChange('signin')} className = 'f3 link dim pa3 ba2 br-pill pointer shadow-5'> Sign In </p>
-                <p onclick = {() => onRouteChange('register')} className = 'f3 link dim pa3 ba2 br-pill pointer shadow-5'> Register </p>
+                <p onClick = {() => onRouteChange('signin')} className = 'f3 link dim pa3 ba2 br-pill pointer shadow-5'> Sign In </p>
+                <p onClick = {() => onRouteChange('register')} className = 'f3 link dim pa3 ba2 br-pill pointer shadow-5'> Register </p>
             </nav>
        );
       }
